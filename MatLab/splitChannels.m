8c17be145c13a028a03f37c79e81eca96d5683c3
Stefan Sietzen <stefan.sietzen@gmx.a>
2012-11-08 00:10:13 +0100

    centroid funktion erstellt
    funktioniert nur, wenn der rote punkt hell genug ist und sonst keine
    roten bereiche im bild sind.

--- MatLab/splitChannels.m
+++ MatLab/splitChannels.m
@@ -20,11 +20,17 @@ resGreen=input(:,:,2)-input(:,:,1);
 resGreen(:,:) = max(min(255,resGreen(:,:)), 0);
 resBlue=input(:,:,3)-input(:,:,1);
 resBlue(:,:) = max(min(255,resBlue(:,:)), 0);
+
+center = centroid(resRed);
 imshow(resRed);
+hold on;
+plot(center(2), center(1), 'o');
+hold off;
+
 uiwait;
 
 resGrey = input;
 resGrey = resGrey(:,:,1)*0.299+resGrey(:,:,2)*0.587+resGrey(:,:,3)*0.114;
-pathStart=[1 1];
+pathStart=center;
 
 end
